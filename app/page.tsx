2f659f3c58a4d5a9be511158c8cc4bf5faa596cb
Maria Clarissa Dolor <23-1-02027@vsu.edu.ph>
2025-10-22 10:19:25 +0800

    added tip selector feature

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@
 
 import { useState } from "react";
 import BillInput from "@/components/BillInput";
+import TipSelector from "@/components/TipSelector";
 
 export default function Home() {
   const [bill, setBill] = useState<number>(0);
@@ -21,6 +22,7 @@ export default function Home() {
     <main className="bg-white w-[900px] rounded-2xl shadow-lg p-8 grid md:grid-cols-2 gap-6">
       <div className="space-y-6">
         <BillInput bill={bill} setBill={setBill} />
+        <TipSelector tip={tip} setTip={setTip} />
       </div>
 
       <div className="bg-[#00474b] rounded-2xl p-8 flex flex-col justify-between">
